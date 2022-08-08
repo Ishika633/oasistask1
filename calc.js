0618f71902ab7c9a9ba9c385602fe4e947616374
Ishika633 <85387056+Ishika633@users.noreply.github.com>
2022-08-08 18:42:14 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,13 @@
+function clr(){
+    document.getElementById("content").value=" ";
+}
+function display(value)
+{
+    document.getElementById("content").value+=value;
+}
+function equal()
+{
+    var content=document.getElementById("content").value;
+    var res=eval(content);
+    document.getElementById("content").value=res;
+}
